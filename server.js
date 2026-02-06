4e72b7ccd6466055277d0b500ddb357f6c8602ed
Roberto <roberto0953@gmail.com>
2026-02-07 00:45:10 +0100

    feat: cattura del codice di autorizzazione nella callback

--- server.js
+++ server.js
@@ -4,3 +4,17 @@ const url = `https://github.com/login/oauth/authorize?client_id=${process.env.GI
 res.redirect(url);
 
 });
+
+
+app.get("/api/auth/github/callback", (req, res) =>{
+
+const  code = req.query.code;
+
+if(!code){
+    return res.status(400).send("Bad request");
+}
+
+console.log(code);
+res.send("Autenticazione riuscita!");
+
+});
